0da6fd091a3189b9f7c3a517b232348ccedeffc4
RenataIrinyi <reniirinyi@icloud.com>
2023-06-21 10:48:39 +0200

    setup basic front-end and finish the Rest API

---
+++ backend/src/main/java/com/backend/config/RestConfiguration.java
@@ -0,0 +1,26 @@
+package com.backend.config;
+
+import com.backend.entity.Product;
+import com.backend.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+@Configuration
+public class RestConfiguration implements RepositoryRestConfigurer {
+
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        HttpMethod[] unsupportedMethods={HttpMethod.DELETE, HttpMethod.PUT, HttpMethod.POST};
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedMethods))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedMethods));
+
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedMethods))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedMethods));
+
+
+    }
+}
